3db1ad52bb6a50e49af9c535705b73b51a4e5944
svitlohot <igor.grinevich@gmail.com>
2026-05-16 10:52:16 +0300

    Update update_bot.py

--- update_bot.py
+++ update_bot.py
@@ -4,9 +4,6 @@ import requests
 CLIENT_ID = os.getenv('SP_ID')
 CLIENT_SECRET = os.getenv('SP_SECRET')
 
-# !!! СЮДИ ВСТАВ ID СВОГО БОТА (цифри з адресного рядка, які йдуть після /telegram/)
-BOT_ID = "69df2033797a9b2192092f5f" 
-
 # !!! СЮДИ ВСТАВ СВІЙ CONTACT_ID (довгий рядок літер та цифр із картки твого контакту)
 CONTACT_ID = "69df205225c75c5ed3048310" 
 
@@ -31,20 +28,22 @@ def get_rates():
 def update_variable(token, var_name, var_value):
     headers = {'Authorization': f'Bearer {token}'}
     payload = {
-        "contact_id": CONTACT_ID,
         "variables": [
-            {"name": var_name, "value": var_value}
+            {
+                "name": var_name,
+                "value": var_value
+            }
         ]
     }
-    # ТЕПЕР URL ПРАВИЛЬНИЙ: ми чітко вказуємо ID бота в самому шляху запиту
-    url = f'https://api.sendpulse.com/telegram/bots/{BOT_ID}/contacts/set-variable'
+    # УНІВЕРСАЛЬНИЙ URL: Він працює без ID бота в самому посиланні!
+    url = f'https://api.sendpulse.com/messengers/contacts/{CONTACT_ID}/variables'
     response = requests.post(url, headers=headers, json=payload)
     print(f"Результат оновлення змінної: {response.status_code} - {response.text}")
 
 token = get_token()
 if token:
     rate_string = get_rates()
-    # Відправляємо дані в нашу нову змінну rate_usd
+    # Оновлюємо нашу змінну rate_usd
     update_variable(token, 'rate_usd', rate_string)
 else:
     print("Не вдалося отримати токен доступу SendPulse")
