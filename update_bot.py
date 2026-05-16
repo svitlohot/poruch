15bdca98ae6d86171ec7233abb4dec6716b4b309
svitlohot <igor.grinevich@gmail.com>
2026-05-16 10:49:49 +0300

    Update update_bot.py

--- update_bot.py
+++ update_bot.py
@@ -4,7 +4,10 @@ import requests
 CLIENT_ID = os.getenv('SP_ID')
 CLIENT_SECRET = os.getenv('SP_SECRET')
 
-# !!! СЮДИ ВСТАВ СВІЙ CONTACT_ID (довгий рядок літер та цифр із адресного рядка картки контакту)
+# !!! СЮДИ ВСТАВ ID СВОГО БОТА (цифри з адресного рядка, які йдуть після /telegram/)
+BOT_ID = "69df2033797a9b2192092f5f" 
+
+# !!! СЮДИ ВСТАВ СВІЙ CONTACT_ID (довгий рядок літер та цифр із картки твого контакту)
 CONTACT_ID = "69df205225c75c5ed3048310" 
 
 def get_token():
@@ -33,15 +36,15 @@ def update_variable(token, var_name, var_value):
             {"name": var_name, "value": var_value}
         ]
     }
-    # Оновлений, стандартний метод API для чат-ботів
-    url = 'https://api.sendpulse.com/telegram/contacts/set-variable'
+    # ТЕПЕР URL ПРАВИЛЬНИЙ: ми чітко вказуємо ID бота в самому шляху запиту
+    url = f'https://api.sendpulse.com/telegram/bots/{BOT_ID}/contacts/set-variable'
     response = requests.post(url, headers=headers, json=payload)
     print(f"Результат оновлення змінної: {response.status_code} - {response.text}")
 
 token = get_token()
 if token:
     rate_string = get_rates()
-    # ТУТ МАЄ БУТИ rate_usd ЗАМІСТЬ exchange_rate
-    update_variable(token, 'rate_usd', rate_string) 
+    # Відправляємо дані в нашу нову змінну rate_usd
+    update_variable(token, 'rate_usd', rate_string)
 else:
     print("Не вдалося отримати токен доступу SendPulse")
